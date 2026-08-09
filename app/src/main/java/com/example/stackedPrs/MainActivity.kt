ad6190a065da6373edf0f03fa2247b406129bc23
yehia2030 <yehia.essa2030@gmail.com>
2026-08-09 04:05:48 +0300

    first screen commit (#1)
    * first screen commit
    
    * adding another commit

--- app/src/main/java/com/example/stackedPrs/MainActivity.kt
+++ app/src/main/java/com/example/stackedPrs/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.stackedPrs.ui.firstScreen.FirstScreen
 import com.example.stackedPrs.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    FirstScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
---
+++ app/src/main/java/com/example/stackedPrs/ui/firstScreen/FirstScreen.kt
@@ -0,0 +1,20 @@
+package com.example.stackedPrs.ui.firstScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.stackedPrs.R
+
+@Composable
+fun FirstScreen(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Launcher Background"
+        )
+    }
+}
