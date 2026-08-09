220cf2e85a3167679baf440fd8d26f11a1bb35d0
Yehia Essa <yehia.essa2030@gmail.com>
2026-08-09 04:13:42 +0300

    third stack screen commit

---
+++ app/src/main/java/com/example/stackedPrs/ui/testStack/ThirdStackScreen.kt
@@ -0,0 +1,9 @@
+package com.example.stackedPrs.ui.testStack
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ThirdStackScreen(modifier: Modifier = Modifier) {
+
+}
